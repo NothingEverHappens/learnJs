d27954cf1049de93c04457e847db544831edf962
voicetime <iprovod@gmail.com>
2016-05-17 23:15:06 +0300

    Make test object props
    Автор косякнул  он использовал
    В качестве ключа индекс массива
    <div key={index}>
    А это плохо и не правильно потому, что массив может измениться , а
    индексы будут те же.
    https://habrahabr.ru/post/279249/#comment_8807873
    Надо узнать как делать уникальные индексы.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
 /**
  * Created by netkacila on 16.05.16.
  */
+
+var myNews = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четчерг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 35 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 var News = React.createClass({
     render: function(){
+        var data = this.props.data;
+        var newsTemplate = data.map(function(item, index){
+            return(
+                <div key={index}>
+                    <p className="news__author">{item.author}</p>
+                    <p className="news_text">{item.text}</p>
+                </div>
+            )
+            });
+        console.log(newsTemplate);
+
+
         return  (
           <div className="news">
-            К сожалению, новостей нет.
+              {newsTemplate}
           </div>
         );
     }
@@ -26,7 +54,7 @@ var App = React.createClass({
         return (
             <div className="app">
                 Всем привет, я компонент App! Я умею отображать новости.
-                <News />
+                <News data={myNews} /> {/*WOW  странно пишутся коменты */}
                 <Comments />
             </div>
         );
