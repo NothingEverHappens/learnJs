394f98bca7de04a63d8c28a5068666342d72011b
voicetime <iprovod@gmail.com>
2016-06-06 01:23:50 +0300

    Make Add card elements

--- js/app.js
+++ js/app.js
@@ -17,6 +17,7 @@ var contacs = [
 
 ];
 
+window.ee = new EventEmitter();
 var Card = React.createClass({
    render: function() {
    var data = this.props.data;
@@ -26,9 +27,10 @@ var Card = React.createClass({
         <div className="cards" key={index}>
           <p className="card__name">{item.name}</p>
           <p className="card__surname">{item.surname}</p>
-          <p>{item.adress}</p>
-          <p>{item.tel}</p>
-          <p>{item.email}</p>
+          <p className="card__adress">{item.adress}</p>
+          <p className="card__tel">{item.tel}</p>
+          <p className="card__email">{item.email}</p>
+
         </div>
       )
     });
@@ -42,18 +44,113 @@ var Card = React.createClass({
     }
 });
 
+var Add = React.createClass({
+    getInitialState: function() {
+      return{
+          agreeNotChecked: true,
+          authorIsEmpty: true,
+          textIsEmpty: true
+      }
+    },
 
+    componentDidMount:  function(){
+      ReactDOM.findDOMNode(this.refs.name).focus();
+    },
+  onBtnClickHandler: function(e) {
+    e.preventDefault();
+    var name = ReactDOM.findDOMNode(this.refs.name).value;
+    var surname = ReactDOM.findDOMNode(this.refs.surname).value;
+    var adress = ReactDOM.findDOMNode(this.refs.adress).value;
+    var tel = ReactDOM.findDOMNode(this.refs.tel).value;
+    var email = ReactDOM.findDOMNode(this.refs.email).value;
 
+    var item = [{
+      name: name,
+      surname: surname,
+      adress: adress,
+      tel: tel,
+      email:email
+    }];
+
+      window.ee.emit('Card.add', item);
+    },
+
+    render: function() {
+        return (
+            <form className='add'>
+                    <input
+                      type='text'
+                      className='add__name'
+                      defaultValue=''
+                      placeholder='Ваше имя'
+                      ref='name'
+
+                    />
+                    <input
+                      className='add__surname'
+                      defaultValue=''
+                      placeholder='Ваша фамилия'
+                      ref='surname'
+                    />
+                     <input
+                      className='add__adress'
+                      defaultValue=''
+                      placeholder='Ваш адрес'
+                      ref='adress'
+                    />
+                    <input
+                      className='add__tel'
+                      defaultValue=''
+                      placeholder='Ваш телефон'
+                      ref='tel'
+                    />
+                    <input
+                      className='add__email'
+                      defaultValue=''
+                      placeholder='Ваша фамилия'
+                      ref='email'
+                    />
+
+                    <button
+                      className='add__btn'
+                      onClick={this.onBtnClickHandler}
+                      ref='alert_button'
+                      >
+                      Создать контакт
+                    </button>
+                  </form>
+             );
+    },
+
+});
 
 
 
 
 
 var App = React.createClass({
+        getInitialState: function() {
+        return {
+            card: contacs
+        };
+    },
+
+  componentDidMount: function() {
+    var self = this;
+    window.ee.addListener('Card.add', function(item) {
+      var nextCard = item.concat(self.state.card);
+      self.setState({card: nextCard});
+    });
+  },
+  componentWillUnmount: function() {
+    window.ee.removeListener('Card.add');
+  },
+
   render: function() {
     return (
       <div className="app">
-        <Card data={contacs} />
+        <Add/>
+        <Card data={this.state.card} />
       </div>
     );
   }
