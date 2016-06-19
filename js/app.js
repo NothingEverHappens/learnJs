ca7cb0b789e6fec44fe25cada64fedd304ecbc60
voicetime <iprovod@gmail.com>
2016-06-19 22:26:11 +0300

    Format indents and lines

--- js/app.js
+++ js/app.js
@@ -13,30 +13,28 @@ var contacs = [
         tel:'+134554334',
         email:'trolo@gmail.com'
     }
-
 ];
 
 window.ee = new EventEmitter();
 var Card = React.createClass({
-   render: function() {
-    var conact_card = this.props.data.map(function(item, index) {
-      return (
-        <div className="cards" key={index}>
-          <p className="card__name">Имя: {item.name}</p>
-          <p className="card__surname">Фамилия: {item.surname}</p>
-          <p className="card__adress">Адрес: {item.adress}</p>
-          <p className="card__tel">Телефон: {item.tel}</p>
-          <p className="card__email">Email: {item.email}</p>
+    render: function() {
+        var conact_card = this.props.data.map(function(item, index) {
+          return (
+            <div className="cards" key={index}>
+              <p className="card__name">Имя: {item.name}</p>
+              <p className="card__surname">Фамилия: {item.surname}</p>
+              <p className="card__adress">Адрес: {item.adress}</p>
+              <p className="card__tel">Телефон: {item.tel}</p>
+              <p className="card__email">Email: {item.email}</p>
+            </div>
+          )
+        });
 
-        </div>
-      )
-    });
-
-       return (
-      <div className="card">
-        {conact_card}
-      </div>
-    );
+        return (
+          <div className="card">
+            {conact_card}
+          </div>
+        );
 
     }
 });
@@ -49,34 +47,32 @@ var Add = React.createClass({
           textIsEmpty: true
       }
     },
-
     componentDidMount:  function(){
       ReactDOM.findDOMNode(this.refs.name).focus();
     },
-  onBtnClickHandler: function(e) {
-    e.preventDefault();
-    var name = ReactDOM.findDOMNode(this.refs.name);
-    var surname = ReactDOM.findDOMNode(this.refs.surname);
-    var adress = ReactDOM.findDOMNode(this.refs.adress);
-    var tel = ReactDOM.findDOMNode(this.refs.tel);
-    var email = ReactDOM.findDOMNode(this.refs.email);
 
-    var item = [{
-      name: name.value,
-      surname: surname.value,
-      adress: adress.value,
-      tel: tel.value,
-      email:email.value
-    }];
+    onBtnClickHandler: function(e) {
+        e.preventDefault();
+        var name = ReactDOM.findDOMNode(this.refs.name);
+        var surname = ReactDOM.findDOMNode(this.refs.surname);
+        var adress = ReactDOM.findDOMNode(this.refs.adress);
+        var tel = ReactDOM.findDOMNode(this.refs.tel);
+        var email = ReactDOM.findDOMNode(this.refs.email);
+        var item = [{
+          name: name.value,
+          surname: surname.value,
+          adress: adress.value,
+          tel: tel.value,
+          email:email.valueъ
+        }];
 
-      window.ee.emit('Card.add', item);
-      name.value = '';
-      surname.value = '';
-      adress.value = '';
-      tel.value = '';
-      email.value = '';
+        window.ee.emit('Card.add', item);
+        name.value = '';
+        surname.value = '';
+        adress.value = '';
+        tel.value = '';
+        email.value = '';
     },
-
     render: function() {
         return (
             <form className='add'>
@@ -124,36 +120,34 @@ var Add = React.createClass({
 
 });
 
-
-
-
-
 var App = React.createClass({
-        getInitialState: function() {
+    getInitialState: function() {
         return {
             card: contacs
-        };
+            };
     },
 
-  componentDidMount: function() {
-    var self = this;
-    window.ee.addListener('Card.add', function(item) {
-      var nextCard = item.concat(self.state.card);
-      self.setState({card: nextCard});
-    });
-  },
-  componentWillUnmount: function() {
-    window.ee.removeListener('Card.add');
-  },
+    componentDidMount: function() {
+        var self = this;
+        window.ee.addListener('Card.add', function(item) {
+          var nextCard = item.concat(self.state.card);
+          self.setState({card: nextCard});
+            });
+    },
 
-  render: function() {
-    return (
-      <div className="app">
-        <Add/>
-        <Card data={this.state.card} />
-      </div>
-    );
-  }
+    componentWillUnmount: function() {
+        window.ee.removeListener('Card.add');
+    },
+
+    render: function() {
+        return (
+          <div className="app">
+            <Add/>
+            <Card data={this.state.card} />
+          </div>
+            );
+    }
+
 });
 
 ReactDOM.render(
